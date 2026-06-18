e0698bb63d2c703dc1ad4c17caeb11e13a7b8499
oussama balti <105465407+baltioussama92@users.noreply.github.com>
2026-06-18 17:22:04 +0100

    fix hosting

--- Frontend/src/api/apiClient.ts
+++ Frontend/src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+export const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL || "http://localhost:8080";
 export const AUTH_TOKEN_KEY = 'authToken'
 const USER_STORAGE_KEY = 'user'
 const ROLE_STORAGE_KEY = 'userRole'
